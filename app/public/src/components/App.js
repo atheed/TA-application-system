a10ab24f5c2209b5c29c69d558135a843cd6125d
Atheed Thameem <atheedthameem@hotmail.com>
2016-11-25 04:20:15 -0500

    added rudimentary routing with React components

--- app/public/src/components/App.js
+++ app/public/src/components/App.js
@@ -1,4 +1,8 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { Router, Route, Link, IndexRoute, 
+    hashHistory, browserHistory } from 'react-router';
+
+import Home from './Home';
 import LoginForm from './LoginForm';
 import SignupForm from './SignupForm';
 
@@ -9,9 +13,11 @@ class App extends Component {
 
     render() {
         return (
-            <div>
-                <LoginForm />
-            </div>
+            <Router history={hashHistory}>
+                <Route path='/' component={Home}/>
+                <Route path='/login' component={LoginForm} />
+                <Route path='/signup' component={SignupForm} />
+            </Router>
         );
     }
 }
--- app/public/src/components/LoginForm.js
+++ app/public/src/components/LoginForm.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import Form from './Form';
 
 class LoginForm extends Component {

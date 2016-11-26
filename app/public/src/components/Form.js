c29ac1ac68dcc64b49292d190c2c936a08f4a434
Atheed Thameem <atheedthameem@hotmail.com>
2016-11-25 20:07:56 -0500

    added option to log in as different types of users

--- app/public/src/components/Form.js
+++ app/public/src/components/Form.js
@@ -6,16 +6,19 @@ class Form extends Component {
     }
 
     render() {
-        const { action } = this.props;
+        const { action, type } = this.props;
         return (
             <form action={action} method="post">
                 <div>
-                    <label>Student Number</label>
+                    <label>{type == "student" ? "Student Number" : "Admin Username"}</label>
                     <input type="text" className="form-control" name="studentnumber"></input>
                 </div>
                 <div>
                     <label>Password</label>
                     <input type="password" className="form-control" name="password"></input>
+                </div>
+                <div>
+                    <input type="hidden" className="form-control" name="type" value={type}></input>
                 </div>
                 <button type="submit">Enter</button>
             </form>
--- app/public/src/components/LoginForm.js
+++ app/public/src/components/LoginForm.js
@@ -4,15 +4,30 @@ import Form from './Form';
 class LoginForm extends Component {
     constructor() {
         super();
+        this.state = {
+            selectedValue: "student"
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({selectedValue: event.target.value});
     }
 
     render() {
         return (
             <div>
-            <h1>Login</h1>
-            <Form 
-                action="/login"
-            />
+                <h1>Login</h1>
+                Login as: 
+                <select defaultValue={this.state.selectedValue} onChange={this.handleChange}>
+                    <option value="student">Student</option>
+                    <option value="admin">Administrator</option>
+                </select>
+                <p />
+                <Form 
+                    action="/login"
+                    type={ this.state.selectedValue }
+                />
             </div>
         );
     }

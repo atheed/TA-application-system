3e7d47fbfe5adb360961e9224d878b005f12cbb1
Evan Klein <evan.klein@hotmail.com>
2016-11-27 14:11:04 -0500

    specified foreign keys in table definitions

--- tapp.sql
+++ tapp.sql
@@ -10,7 +10,7 @@ CREATE TABLE Login (
 );
 
 CREATE TABLE Applicants (
-  StudentNumber VARCHAR PRIMARY KEY,
+  StudentNumber VARCHAR PRIMARY KEY REFERENCES Applicants(StudentNumber),
   FamilyName VARCHAR,
   GivenName VARCHAR,
   Year INTEGER,
@@ -22,18 +22,31 @@ CREATE TABLE Courses (
   Code VARCHAR PRIMARY KEY,
   Description VARCHAR,
   Instructor VARCHAR,
-  NumberOfTAs INTEGER,
-  Degree VARCHAR
+  NumberOfTAs INTEGER
 );
 
 CREATE TABLE Offers (
-  StudentNumber VARCHAR PRIMARY KEY,
-  CourseCode VARCHAR UNIQUE
+  StudentNumber VARCHAR PRIMARY KEY REFERENCES Applicants(StudentNumber),
+  CourseCode VARCHAR REFERENCES Courses(Code)
 );
 
-CREATE TABLE Applications (
-  StudentNumber VARCHAR PRIMARY KEY,
-  CourseCode VARCHAR,
+CREATE TABLE Rankings (
+  StudentNumber VARCHAR REFERENCES Applicants(StudentNumber),
+  CourseCode VARCHAR REFERENCES Courses(Code),
   Rank INTEGER,
-  Experience INTEGER
+  Experience INTEGER,
+  PRIMARY KEY (StudentNumber, CourseCode)
 );
+
+insert into applicants values('1000831745', 'Klein', 'Evan', 4, 'Undergrad', '');
+insert into applicants values('1000123456', 'Chaudhary', 'Rahul', 4, 'Grad', '');
+
+insert into courses values('CSC108', 'Intro to CS', 'J Smith', 50);
+insert into courses values('CSC120', 'Intro to CS for Science', 'Prof 2', 25);
+
+insert into rankings values('1000831745', 'CSC108', 1, 0);
+
+insert into rankings values('1000123456', 'CSC120', 1, 0);
+
+insert into rankings values('1000123456', 'CSC108', 2, 0);
+

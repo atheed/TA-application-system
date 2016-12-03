973874f4fdf8f3c6d0761f185d51dbf7512574ca
Atheed Thameem <atheedthameem@hotmail.com>
2016-12-03 02:41:47 -0500

    fixed some styling on the profile page, and added new fields, with the appropriate onSubmit handler

--- app/public/src/components/Profile.js
+++ app/public/src/components/Profile.js
@@ -5,6 +5,9 @@ import AutosuggestBox from './AutosuggestBox';
 import Skills from './Skills';
 var styles = require('./../../client/css/profile.css');
 
+var utils = require('../utils.js');
+var json = utils.json;
+
 let skillsList = [];
 
 class Profile extends Component {
@@ -18,6 +21,7 @@ class Profile extends Component {
         };
 
         this.handleAutosuggestChoice = this.handleAutosuggestChoice.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleAutosuggestChoice(newSkill) {
@@ -25,24 +29,89 @@ class Profile extends Component {
         this.forceUpdate();
     }
 
+    handleSubmit(event) {
+        event.preventDefault();
+
+        // TODO
+        // get the student number through some sort of request
+
+        // build request for form submit
+        var params = {
+            StudentNumber: "",
+            FamilyName: this.refs.familyname.value,
+            GivenName: this.refs.givenname.value,
+            Status: this.refs.status.value,
+            Year: this.refs.year.value,
+            Eligibility: this.refs.eligibility.value,
+            Qualifications: skillsList,
+            AdditionalInfo: this.refs.additionalInfo.value
+        };
+        const searchParams = Object.keys(params).map((key) => {
+            return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+        }).join('&');
+        
+        // make form submit (POST) request
+        fetch("/TODO", {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                },
+                body: searchParams
+            })
+            .then(json)
+            .then(function(data) {
+                // if unsuccessful form submit (i.e. incorrect credentials, etc.), throw an error
+                if (!data.success)
+                    throw "error";
+
+                hashHistory.push('/courseselection');
+            })
+            .catch(function(err) {
+                throw err;
+            });
+    }
+
     render() {
         return (
             <div id="formEntry">
                 <h1 id="profileHeading">Student Profile</h1>
-                <form action="/TODO" method="post">
+                <form onSubmit={this.handleSubmit} className="form-horizontal">
+                    <div>
+                        <label className="formLabel">Family Name</label>
+                        <input type="text" className="form-control" name="familyname" ref="familyname"></input>
+                    </div>
+                    <p></p>
+                    <div>
+                        <label className="formLabel">First Name</label>
+                        <input type="text" className="form-control" name="givenname" ref="givenname"></input>
+                    </div>
+                    <p />
                     <div>
                         <label className="formLabel">Degree Status</label>
                         <br />
-                        <select name="status">
+                        <select name="status" ref="status">
                             <option value="undergrad">Undergraduate</option>
                             <option value="grad">Graduate</option>
                         </select>
                     </div>
                     <p />
                     <div>
+                        <label className="formLabel">Year</label>
+                        <br />
+                        <select name="year" ref="year">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+                    <p />
+                    <div>
                         <label className="formLabel">Work Eligibility</label>
                         <br />
-                        <select name="eligibility">
+                        <select name="eligibility" ref="eligibility">
                             <option value="legal">Legally Entitled</option>
                             <option value="student">Student Visa</option>
                         </select>
@@ -57,7 +126,7 @@ class Profile extends Component {
                     <div>
                         <label className="formLabel">Additional Info:</label>
                         <br />
-                        <textarea name="additional-info"></textarea>
+                        <textarea name="additional-info" ref="additionalInfo"></textarea>
                         </div>
                     <p />
                     <button type="submit">Enter</button>

d5dda5b2796ea9d7af3844dc336eb87e8510a613
Atheed Thameem <atheedthameem@hotmail.com>
2016-12-03 21:49:48 -0500

    created navbar with appropriate links, at the top of each student page

--- app/public/src/components/AutosuggestBox.js
+++ app/public/src/components/AutosuggestBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Select from 'react-select';
 
 import 'react-select/dist/react-select.css';
+var styles = require('./../../client/css/autosuggest.css');
 
 var skillsList = require('./../skillsList.js');
 
@@ -25,14 +26,16 @@ export default class AutosuggestBox extends Component {
 
     render() {
         return(
+            <div className="autosuggest">
             <Select 
                 multi 
                 simpleValue 
                 value={this.state.value} 
-                placeholder="Enter a skill" 
+                placeholder="Enter some skills" 
                 options={skillsList.skillsList} 
                 onChange={this.updateValue} 
             />
+            </div>
         );
     }
 }

2bf03e82bde30d79359939c3e1d12d99bc0edaa7
Evan Klein <evan.klein@hotmail.com>
2016-12-04 04:23:02 -0500

    Added experience ticker in cart view for each course

---
+++ app/public/src/components/Experience.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+
+var utils = require('../utils.js');
+
+var json = utils.json;
+
+class Experience extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            experience: String(props.rank),
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        // make post request
+        let newExperience = event.target.value;
+
+        var t = this;
+
+        fetch('/update-experience-in-course', { 
+            method: 'POST', 
+            credentials: 'include',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              // stunum: "1000831745", // TODO: 
+              course: this.props.course,
+              experience: newExperience
+            })          
+        })
+            .then(json)
+            .then(function(data) {
+                t.setState({
+                  experience: newExperience
+                });
+                return;
+            })
+            .catch(function(err) {
+                // Error :(
+                throw err;
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <label className="experienceField">
+                    <span className="label">Experience:</span>
+                    <input name="experience" type="number" onChange={this.handleChange} required/>
+                </label>
+            </div>
+        );
+    }
+}
+
+export default Experience;
